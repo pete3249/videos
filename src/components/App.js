a98dec3c641e6b9587eae56785f5017f293a5a12
pete3249 <katelynrpeterson16@gmail.com>
2021-05-09 23:03:03 -0400

    Add App Component

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SearchBar from './SearchBar'
+
+class App extends React.Component {
+    render() {
+        return (
+            <div classname="ui container">
+                <SearchBar />
+            </div>
+        )
+    }
+}
+
+export default App;
