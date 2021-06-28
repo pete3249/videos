1be652aa1e7e1bd2606eb44c01b4072740657768
pete3249 <katelynrpeterson16@gmail.com>
2021-06-27 21:27:04 -0400

    Configure API KEY

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,16 +1,14 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyAkUigfMFCGSG75_nZg87nUsFav3QbnUeA'
+const key = process.env.REACT_APP_API_KEY
 
-// creating an axios instance
-// going to pass in query string when we actually make use of this method
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
     params: {
         part: 'snippet',
         type: 'video',
         maxResults: 5,
-        key: KEY
+        key: key
     }
 });
 
